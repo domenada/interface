b41c42cd9270748906e6bae5896434e34d0f074a
domenada <adventurersuper@gmail.com>
2019-08-04 21:40:54 -0400

    Lesson_1

---
+++ vk_app_1/vk_app_1Tests/vk_app_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  vk_app_1Tests.swift
+//  vk_app_1Tests
+//
+//  Created by Dmitry Andreyanov on 8/4/19.
+//  Copyright © 2019 Dmitrii Andreianov. All rights reserved.
+//
+
+import XCTest
+@testable import vk_app_1
+
+class vk_app_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
