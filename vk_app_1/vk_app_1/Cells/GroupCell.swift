198db15fdc1fe9b961ddaeb0ca1a5792fc3345a0
domenada <adventurersuper@gmail.com>
2019-08-14 17:51:55 -0400

    Приложение компилируется, но крашится при переходе на вкладку Группы в Таб контроллере. Какие-то проблемы с настройкой ячейки, я так и не разобрался

---
+++ vk_app_1/vk_app_1/Cells/GroupCell.swift
@@ -0,0 +1,13 @@
+//
+//  GroupCell.swift
+//  vk_app_1
+//
+//  Created by Dmitry Andreyanov on 8/13/19.
+//  Copyright © 2019 Dmitrii Andreianov. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+    @IBOutlet weak var groupAvatarView: UIImageView!
+    @IBOutlet weak var groupNameLabel: UILabel!}
---
+++ vk_app_1/vk_app_1/Models/GroupModel.swift
@@ -0,0 +1,17 @@
+//
+//  GroupModel.swift
+//  vk_app_1
+//
+//  Created by Dmitry Andreyanov on 8/13/19.
+//  Copyright © 2019 Dmitrii Andreianov. All rights reserved.
+//
+
+
+import UIKit
+
+struct Group {
+    let name: String
+    let avatar: UIImage?
+   
+   
+}
